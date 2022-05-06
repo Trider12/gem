7d66825eb5d609c8d36953fa384fd5cf9663c99a
Trider <47952308+Trider@users.noreply.github.com>
2022-05-07 00:49:58 +0600

    Removed SDL headers and binaries, added OpenSSL headers and binaries, set SDL to be built by Vcpkg

---
+++ tools/scripts/vcpkg_install_third_parties.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)) + "/../vcpkg")
+
+
+def run(cmd):
+    subprocess.check_call(cmd, shell=True)
+
+
+if os.name == 'nt':
+    run(["bootstrap-vcpkg.bat", "--disable-metrics"])
+    run("vcpkg.exe install sdl2:x64-windows")
+else:
+    run(["./bootstrap-vcpkg.sh", "--disable-metrics"])
+    run("./vcpkg install sdl2")
